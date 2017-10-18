0348d49adecabdb25b97759b334354a1cdd15fff
chiksoon <soonnean.chik@gmail.com>
2017-10-18 10:58:45 +0100

    add backend dir

---
+++ backend/book.py
@@ -0,0 +1,70 @@
+
+# coding: utf-8
+
+# In[4]:
+
+import json
+import pandas as pd
+
+from collections import OrderedDict
+from matplotlib import pyplot as plt
+from matplotlib import animation
+from requests import get
+from datetime import date, datetime
+get_ipython().magic(u'matplotlib inline')
+
+
+# In[5]:
+
+BASE_URI = r'https://api.gemini.com/v1/book/ethusd?'
+API_KEY = r'1234'
+
+
+# In[6]:
+
+def api(asks, bids, **kwargs):
+    kwargs.update({'limit_asks': asks,
+                   'limit_bids': bids
+                  })
+    response = get(BASE_URI, kwargs).json()
+    bid_price, bid_amount, spread = [], [], []
+    for result in response:
+        df = pd.DataFrame(response['asks'])
+#         for row in df:
+#             df['timestamp'] = datetime.fromtimestamp(df['timestamp'].astype(str)).\
+#                                                     strftime('%Y-%m-%d %H:%M:%S')
+
+    for i in response['bids']:
+        bid_price.append(i['price'])
+        bid_amount.append(i['amount'])
+    df['bid_price'] = bid_price
+    df['bid_amount'] = bid_amount
+#     df['timestamp'] = map(lambda x: pd.to_datetime(x, unit='ms'), df['timestamp'])
+    
+    return df
+
+def calc_spread():
+    book = api(5, 5)
+    for row in book:
+        book['price'] = book['price'].astype(float)
+        book['bid_price'] = book['bid_price'].astype(float)
+        book['spread'] = (book['price'] - book['bid_price']).astype(float)
+    
+    print book
+
+def count_neg_spread(book):
+    for i in book['spread']:
+        if i < 0:
+            print 'smiley'
+            
+def main():
+    book = calc_spread()
+
+if __name__ == '__main__':
+    main()
+
+
+# In[53]:
+
+
+
---
+++ backend/lstm.py
@@ -0,0 +1,56 @@
+
+# coding: utf-8
+
+# In[6]:
+
+import time
+import warnings
+import book
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.cross_validation import  train_test_split
+from matplotlib import pyplot as plt
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential
+
+get_ipython().magic(u'matplotlib inline')
+
+
+# In[ ]:
+
+def build_model(layers):
+    model = Sequential()
+
+    model.add(LSTM(
+        input_dim = layers[0],
+        output_dim = layers[1],
+        return_sequences = True))
+    model.add(Dropout(0.2))
+
+    model.add(LSTM(
+        layers[2],
+        return_sequences = False))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(
+        output_dim = layers[3]))
+    model.add(Activation("linear"))
+
+    start = time.time()
+    model.compile(loss="mse", optimizer="rmsprop")
+    print("> Compilation Time : ", time.time() - start)
+    
+    return model
+
+def normalise_windows(window_data):
+    normalised_data = []
+    for window in window_data:
+        for p in window:
+            normalised window = float(p) / float(window[0]) - 1
+        normalised_data.append(normalised_window)
+    
+    return normalised_data
+
