0c52719951a7bdba7b4ce97ba3b0a38737edfe04
chiksoon <soonnean.chik@gmail.com>
2017-10-18 17:56:16 +0100

    update run script

--- process/run.py
+++ process/run.py
@@ -1,17 +1,11 @@
-
-# coding: utf-8
-
-# In[3]:
-
 import threading
 from threading import Thread
 from book import api_book, calc_spread
 from trades import api_trades
 
-
-# In[ ]:
-
-if __name__ == '__main__':
+def main():
     Thread(target = api_book).start()
     Thread(target = api_trades).start()
 
+if __name__ == '__main__':
+    main()
